6fb334ae7de8cffd1c42461ff231afc06ff9a7fa
Shubham Sharma <shubham-int@sharefable.com>
2024-02-06 12:23:57 +0530

    update custom layout

--- .components/custom-layout/CustomLayout.jsx
+++ .components/custom-layout/CustomLayout.jsx
@@ -14,23 +14,31 @@ const CustomLayout = (props) => {
     <>
       <Header />
       <Sidepanel />
-      {!isBlogRoute && (
-        <Banner config={props.config}>
-          <div style={{ maxWidth: '768px' }}>
-            {props.frontmatter?.bannerDate || 'Date missing'}
-            <h1 id='banner-title'>{props.frontmatter?.bannerTitle || 'Title missing'}</h1>
-            {props.frontmatter?.bannerSubtitle || 'Subtitle missing'}
+      {(props.frontmatter.bannerTitle || props.frontmatter.title) && (
+        <Banner config={props.config} height={isBlogRoute && '60vh'}>
+          <div style={isBlogRoute ? { maxWidth: '546px', padding: '0 16px' } : { maxWidth: '768px' }}>
+            {props.frontmatter?.bannerDate || props.frontmatter?.date}
+            <h1 id='banner-title'>
+              {props.frontmatter?.bannerTitle || props.frontmatter?.title}
+            </h1>
+            {props.frontmatter?.bannerSubtitle || props.frontmatter?.subtitle}
           </div>
-          <CoverImg src={props.frontmatter?.bannerImg || 'https://github.com/sid-patri-fable/fable-blog/assets/155963795/94565b42-4e6e-49c7-9138-64bdd31baf6e'} />
+          {(props.frontmatter?.bannerImg || props.frontmatter?.ogImg) && (
+            <CoverImg src={props.frontmatter?.bannerImg || props.frontmatter?.ogImg} />
+          )}
         </Banner>
       )}
       {props.children}
-      {!isBlogRoute && (
+      {(props.frontmatter?.promotionTitle || props.frontmatter?.promotionSubtitle) && (
         <Promotion config={props.config}>
-          <h2>Start your 14-day free trial today!</h2>
-          <p>Want to get the right partner for your sales team and close more deals?</p>
-
-          <PromotionBannerCta href="https://app.sharefable.com/login" cta='Get started' />
+          <h2>{props.frontmatter.promotionTitle}</h2>
+          <p>{props.frontmatter.promotionSubtitle}</p>
+          {(props.frontmatter?.promotionLink || props.frontmatter.promotionCTA) && (
+            <PromotionBannerCta 
+              href={props.frontmatter.promotionLink}
+              cta={props.frontmatter.promotionCTA}
+            />
+          )}
         </Promotion>
       )}
       <Footer />
