13e7831965fb6edb9dd3e86316f9f86fa753012c
Shubham Sharma <shubham-int@sharefable.com>
2024-04-15 17:40:53 +0530

    remove retention script

--- analytics.js
+++ analytics.js
@@ -44,17 +44,4 @@ const addGTMandGA = () => {
   }
 };
 
-const retentionB2B = () => {
-  const head = document.querySelector("head");
-
-  const retentionScript = `
-    <script id="retention-script">!function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("5Z6PVLH3JG6R");}();</script>
-  `;
-
-  if (!head.querySelector("script#retention-script")) {
-    head.insertAdjacentHTML("afterbegin", retentionScript);
-  }
-};
-
-retentionB2B();
 addGTMandGA();
